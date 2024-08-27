51f160ef090686c000773376649c4608bc537c40
naolselemon <naol.selemon.scitech@gmail.com>
2024-08-27 16:13:24 +0300

    all are done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is : ${name}`);
-
-  process.stdin.end();
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is : ${data}`);
 });
 
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
